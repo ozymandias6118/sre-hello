c330e583169cbe2256072e2ad5c341509b9949e4
unknown <ozymandias6118.William>
2019-10-28 23:42:11 -0700

    Added line to define the missing grade so that the average grades will sum to 85.

--- bms.c
+++ bms.c
@@ -6,6 +6,7 @@ int main() {
 	
 	grades[0] = 80;
 	/* TODO: define the missing grade so that the average will sum to 85. */
+	grades[1] = 85;
 	grades[2] = 90;
 	average = (grades[0] + grades[1] + grades[2] / 3;
 	printf(The average of the 3 grades is: %d", average);
